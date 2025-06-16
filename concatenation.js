4efb3b640171b2f9cce30313a993aae90dc4e7b9
coeeri001 <coeeri001@warren.k12.in.us>
2025-06-16 14:13:32 -0400

    commit

--- concatenation.js
+++ concatenation.js
@@ -7,11 +7,13 @@ submitButton.addEventListener("click", function() {
     let mood = document.querySelector(".mood").value;
     let food = document.querySelector(".food-name").value;
     let birthYear = document.querySelector(".year").value;
-    let age = 2023 - birthYear; // update to 2024, if necessary
+    let age = 2025 - birthYear; // update to 2024, if necessary
+   let myCity = document.querySelector(".city").value;
     // 1. Declare a variable to save the value of the city the user is from.
 
     // 2. Below write your sentences! 
-    // - Have them show up in the div with class "results".
+let result = "My name is " + myName + " I'm in a " + mood + " mood. " + " So I eat " + food + " to feel better. " +  " Some people question why I eat " + food + " at the big age of " + age + " , and to that I say welcome to "+ myCity;
+results.innerHTML = result    // - Have them show up in the div with class "results".
     // - Example paragraph: "My name is (myName). I'm feeling (mood) and I absolutely lovveeee eating (food) 😍..."
     // - Try placing your sentences into <p> or <li> tags.
 
--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 let buttonOne = document.querySelector(".button-one");
 let captionOne = document.querySelector(".caption-one");
+let buttonTwoElm = document.querySelector(".button-two")
+let captionTwoElm = document.querySelector(".caption-two")
+
 
 buttonOne.addEventListener("click", function() {
+    let spideyCaption = document.querySelector(".comment-one").value; 
     // 1. Declare a variable named spidyCaption.
     //  - Store the value of the input with a class of "comment-one".
 
 
     // 2. Display the variable spidyCaption to captionOne.
+    captionOne.innerHTML = spideyCaption;
+
+
+});
+
+buttonTwoElm.addEventListener("click", function() {
+    let kermitCaption = document.querySelector(".comment-two").value; 
+    // 1. Declare a variable named spidyCaption.
+    //  - Store the value of the input with a class of "comment-one".
+
+
+    // 2. Display the variable spidyCaption to captionOne.
+    captionTwoElm.innerHTML = kermitCaption;
 
 
 });
